276be9f467e135d09f90d43e43b89c9767b2d2fd
jeromeabel <j.abel@allo-media.fr>
2025-10-09 10:38:05 +0200

    chore: add site to config for canonical URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://sp3ctra.io/",
   adapter: netlify(),
-
   vite: {
     plugins: [tailwindcss()],
   },
