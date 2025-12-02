d04f889dbb7b123d1da5e673c3b277376565caa0
jeromeabel <j.abel@allo-media.fr>
2025-12-02 23:03:27 +0100

    feat: add SocialLink component to Header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import netlify from "@astrojs/netlify";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://sp3ctra.io/",
   adapter: netlify(),
+  integrations: [icon()],
 
   vite: {
     plugins: [tailwindcss()],
